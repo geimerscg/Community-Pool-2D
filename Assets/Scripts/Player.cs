ff02bc34844f3e5fa8ecdce6f0885392c72279c2
geimerscg <51518457+geimerscg@users.noreply.github.com>
2020-09-21 15:29:27 -0700

    2d restart

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+
+
+    void CalculateMovement()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 3.82f)
+        {
+            transform.position = new Vector3(transform.position.x, 3.82f, 0);
+        }
+        else if (transform.position.y <= -3.87f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.87f, 0);
+        }
+
+        if (transform.position.x >= 8.18f)
+        {
+            transform.position = new Vector3(8.18f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -8.18f)
+        {
+            transform.position = new Vector3(-8.18f, transform.position.y, 0);
+        }
+
+    }
+}
