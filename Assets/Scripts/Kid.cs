f3d28ca527f63a61f50d1b3c5a4b267614d83e67
geimerscg <51518457+geimerscg@users.noreply.github.com>
2020-09-22 08:45:45 -0700

    added first npc

---
+++ Assets/Scripts/Kid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kid : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make kid travel down lanes
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y <= -5.75f) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _kidPrefab;
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(KidSpawnRoutine());        
+    }
+
+
+    IEnumerator KidSpawnRoutine() 
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        while (true)
+        {
+            Vector3 startingSpot = new Vector3(Random.Range(-8f, 8f), 8f, 0);
+            GameObject newKid = Instantiate(_kidPrefab, startingSpot, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    } 
+
+
+}
